717b3c554328efe6bf14116273b7b9dcee55c597
Faron Norbert <faronnorbertkrk@gmail.com>
2020-12-30 16:54:52 +0100

    while loop has been split into several functions

--- main.c
+++ main.c
@@ -11,13 +11,52 @@
 
 
 void PIT_IRQHandler(void);
-
+char checkTheValue(char oper);
 char input = ' ';
 int i = -1;
 int changeValue = 0;
 double currentValue = 0;
-double savedValue = 20;
+double savedValue = 0;
 char result[16];
+int clear = 0;
+char display[20] = {0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
+char operation = 'A';
+void clearTheValue(void);
+void setNumber(void);
+void displayResultHandler(void);
+char operationValue[2];
+	
+void setNumber(){
+	if(clear){
+						LCD1602_ClearAll();
+						clear=0;
+						LCD1602_SetCursor(0,1);
+		if(operation != 'A'){
+			sprintf(operationValue,"%c",operation);
+						LCD1602_Print(operationValue);
+		}
+						
+					} 
+					result[i] = input;
+					currentValue = atof(result);
+					if( currentValue >= 0 && currentValue == (int64_t)currentValue )
+						sprintf(display,"%llu",(int64_t)currentValue);
+					else
+						sprintf(display,"%f",currentValue);
+
+}
+
+void clearTheValue(){
+
+		operation = 'A';
+		currentValue = 0;
+		savedValue = 0;
+		i=0;
+		LCD1602_ClearAll();
+		sprintf(result,"%s","                ");
+		sprintf(display,"%llu",(int64_t)currentValue);
+
+}
 
 void PIT_IRQHandler()
 {
@@ -30,34 +69,36 @@ void PIT_IRQHandler()
 
   PIT->CHANNEL[0].TFLG = PIT_TFLG_TIF_MASK;
 }
-
-
-int main()
-{
-
-
-	PIT_Init();
-	LCD1602_Init();		 /* initialize LCD		  */
-	keyPadInit();
 	
-	
-	LCD1602_Backlight(TRUE);
-	LCD1602_ClearAll();
+char checkTheValue(char oper){
 
+	if(operation == 'A'){
+						savedValue = currentValue;
+						sprintf(display,"%c",'0');
+						i=0;
+						LCD1602_ClearAll();
+						sprintf(result,"%s","                ");
+					}else{
+						clear=1;
+						LCD1602_ClearAll();
+						currentValue = CalcResult(savedValue,currentValue,operation);
+						savedValue = currentValue;
+						sprintf(result,"%s","                ");
+					if(currentValue >= 0 && currentValue == (int64_t)currentValue)
+						sprintf(display,"%llu",(int64_t)currentValue);
+					else
+						sprintf(display,"%f",currentValue);
+					}
+						LCD1602_SetCursor(0,1);
+						sprintf(operationValue,"%c",oper);
+						LCD1602_Print(operationValue);
+	return oper;
 
-	char display[20] = {0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
-	char operation = 'A';
+}
 
-	LCD1602_SetCursor(0,0);
-	sprintf(display,"0");
-	LCD1602_SetCursor(0,0);
-	LCD1602_Print(display);
+void displayResultHandler(){
 
-  while(1)
-	{
-		
-		if(changeValue){
-			switch(input){
+	switch(input){
 				case '=':
 					if(operation == 'A'){
 						operation = input;
@@ -69,53 +110,54 @@ int main()
 					}
 					break;
 				case '+':
-					if(operation == 'A'){
-					savedValue = currentValue;
-					operation = '+';
-					sprintf(display,"%c",input);
-					i=0;
-					LCD1602_ClearAll();
-					sprintf(result,"%s","                ");
-					}else{
-						LCD1602_ClearAll();
-						sprintf(display,"%f",CalcResult(savedValue,currentValue,operation));
-					}
+					operation = checkTheValue('+');
 					break;
 				case '-':
-					sprintf(display,"%c",input);
-					i=0;
-					LCD1602_ClearAll();
-					sprintf(result,"%s","                ");
+					operation = checkTheValue('-');
+					break;
+				case '*':
+					operation = checkTheValue('*');
 					break;
 				case '/':
-					sprintf(display,"%c",input);
-					i=0;
-					LCD1602_ClearAll();
-					sprintf(result,"%s","                ");
+					operation = checkTheValue('/');
 					break;
 				case 'C':
-					sprintf(display,"%c",input);
-					i=0;
-					LCD1602_ClearAll();
-					sprintf(result,"%s","                ");
+					clearTheValue();
 					break;
 				default:
-					result[i] = input;
-					currentValue = atof(result);
-					if(currentValue == (int64_t)currentValue)
-						sprintf(display,"%llu",(int64_t)currentValue);
-					else
-						sprintf(display,"%f",currentValue);
-
+					setNumber();
 				break;
-			}
-			changeValue = 0;
-			LCD1602_SetCursor(0,0);
-			LCD1602_Print(display);
+}}
+
+
+int main()
+{
+
+
+	PIT_Init();
+	LCD1602_Init();		 /* initialize LCD		  */
+	keyPadInit();
+	LCD1602_Backlight(TRUE);
+	LCD1602_ClearAll();
+	LCD1602_SetCursor(0,0);
+	sprintf(display,"0");
+	LCD1602_SetCursor(0,0);
+	LCD1602_Print(display);
+
+  while(1)
+	{
+		
+		if(changeValue){
+				displayResultHandler();
 		}
-					
+		
+		changeValue = 0;
+		LCD1602_SetCursor(0,0);
+		LCD1602_Print(display);
+		
+		}
 	}
-}
+
 
 
 	
